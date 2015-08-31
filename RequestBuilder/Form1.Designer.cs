ca1cd2d7716773c6331d04d89324900166093e97
Patrick Bell <widoreu.bell@gmail.com>
2015-08-31 16:50:30 -0600

    Initial Commit

---
+++ RequestBuilder/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using Titanium.Web.Proxy.Models;
+
+namespace RequestBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public delegate int addToList(int i, SessionEventArgs e);
+
+        public int addConnection(int i,SessionEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new addToList(addConnection),new object[] { i, e });
+            }
+            else
+            {
+                if (e.ServerResponse == null)
+                {
+                    String url = e.RequestURL;
+                    if (e.RequestURL.IndexOf(e.RequestHostname) != -1)
+                    {
+                        String s = (e.RequestURL.Substring(e.RequestURL.IndexOf(e.RequestHostname) + e.RequestHostname.Length));
+                        url = s.Substring(s.IndexOf("/"));
+                    }
+                    dataGridView1.Rows.Add(e.ProxyRequest.Method,i, "Waiting...", e.RequestHostname, url, "-1");
+                    return dataGridView1.Rows.Count-1;
+                }
+            }
+            return -1;
+        }
+
+        public delegate void modifyFromList(int i, SessionEventArgs e);
+
+        public void modifyFrom(int i, SessionEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new modifyFromList(modifyFrom), new object[] { i, e });
+            }
+            else
+            {
+                if (e.ServerResponse != null)
+                {
+                    dataGridView1.Rows[i].Cells[2].Value = e.ServerResponse.StatusCode.ToString();
+                    dataGridView1.Rows[i].Cells[5].Value = e.ServerResponse.ContentLength;
+                }
+            }
+        }
+
+        public delegate void setInfo(SessionEventArgs e);
+
+        public void setConnection(SessionEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new setInfo(setConnection), new object[] { e });
+            }
+            else
+            {
+                label1.Text = "Hostname: " + e.RequestHostname;
+                label2.Text = "URL: " + e.RequestURL;
+                label3.Text = "Useragent: " + e.ProxyRequest.UserAgent;
+                label4.Text = "Mediatype: " + e.ProxyRequest.MediaType;
+                label5.Text = "Referer: " + e.ProxyRequest.Referer;
+                label6.Text = "Keep alive: " + e.ProxyRequest.KeepAlive;
+                label7.Text = "Request URI: " + e.ProxyRequest.RequestUri;
+                try
+                {
+                    textBox1.Text = e.GetResponseHtmlBody();
+                }
+                catch(Exception exception)
+                {
+                    textBox1.Text = "Could not read response! ["+exception.Message+"]";
+                }
+            }
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hostname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ContentLength = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(857, 665);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(621, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Proxy Server\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Type,
+            this.ID,
+            this.Status,
+            this.Hostname,
+            this.URL,
+            this.ContentLength});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(851, 711);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            // 
+            // Hostname
+            // 
+            this.Hostname.HeaderText = "Hostname";
+            this.Hostname.Name = "Hostname";
+            this.Hostname.ReadOnly = true;
+            // 
+            // URL
+            // 
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.ReadOnly = true;
+            // 
+            // ContentLength
+            // 
+            this.ContentLength.HeaderText = "Content Length";
+            this.ContentLength.Name = "ContentLength";
+            this.ContentLength.ReadOnly = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(857, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(621, 659);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(613, 630);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Connection Info";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(601, 184);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Request Information";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Request URI:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Keep alive:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Referer:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mediatype:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Useragent:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "URL:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hostname:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(613, 630);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Response Info";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(603, 618);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1490, 711);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Type;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn Status;
+        private DataGridViewTextBoxColumn Hostname;
+        private DataGridViewTextBoxColumn URL;
+        private DataGridViewTextBoxColumn ContentLength;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+    }
+}
+
---
+++ RequestBuilder/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Titanium.Web.Proxy.Models;
+using RequestBuilder;
+using System.Net;
+using Titanium.Web.Proxy;
+using System.Threading;
+
+namespace RequestBuilder
+{
+    public partial class Form1 : Form
+    {
+        public List<Tuple<int,SessionEventArgs>> connections = new List<Tuple<int, SessionEventArgs>>();
+        internal int i = 0;
+        internal bool flag = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                ProxyServer.Stop();
+            }
+            catch
+            {
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public bool startProxyServer(bool SSL, bool sysProxy)
+        {
+            ProxyServer.BeforeRequest += OnRequest;
+            ProxyServer.BeforeResponse += OnResponse;
+
+            ProxyServer.EnableSSL = SSL;
+            ProxyServer.SetAsSystemProxy = sysProxy;
+            return ProxyServer.Start();
+        }
+        public void stopProxyServer()
+        {
+            ProxyServer.BeforeRequest -= OnRequest;
+            ProxyServer.BeforeResponse -= OnResponse;
+            ProxyServer.Stop();
+        }
+        public void OnRequest(object sender, SessionEventArgs e)
+        {
+            flag = true;
+            addConnection(i, e);
+            if (connections.Count > 100000)
+            {
+                connections.RemoveAt(0);
+            }
+            connections.Add(new Tuple<int, SessionEventArgs>(i,e));
+            i++;
+            flag = false;
+        }
+        public void OnResponse(object sender, SessionEventArgs e)
+        {
+            foreach (Tuple<int, SessionEventArgs> request in connections)
+            {
+                if (e.ProxyRequest.Equals(request.Item2.ProxyRequest))
+                {
+                    modifyFrom(request.Item1, e);
+                    break;
+                }
+            }
+            if (e.ServerResponse.StatusCode == HttpStatusCode.OK)
+            {
+                if (e.ServerResponse.ContentType.Trim().ToLower().Contains("text/html"))
+                {
+                    if (e.GetResponseHtmlBody() != null)
+                    {
+                        try
+                        {
+                            e.Ok("<!-- Processed by RequestBuilder -->\n" + e.GetResponseHtmlBody());
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (WebRequest.DefaultWebProxy.GetProxy(new Uri("http://www.google.com/")).Host.Equals("localhost"))
+            {
+                stopProxyServer();
+                MessageBox.Show(this, "Proxy server successfully stopped.");
+                button1.Text = "Start Proxy Server";
+            }
+            else
+            {
+                if (startProxyServer(true, true))
+                {
+                    MessageBox.Show(this, "Proxy server successfully started.");
+                }
+                else
+                {
+                    MessageBox.Show(this, "Proxy server failed to start!");
+                }
+                button1.Text = "Stop Proxy Server";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            setConnection(connections[e.RowIndex].Item2);
+        }
+    }
+    class ConnectionView : ListViewItem
+    {
+        public ConnectionView(SessionEventArgs e)
+        {
+            this.Name = e.RequestHostname;
+            this.SubItems.Add(e.RequestHostname);
+        }
+    }
+}
